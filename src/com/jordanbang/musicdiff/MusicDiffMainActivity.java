6d1abccb9014f57425318b32c046d0c7ad2263ab
Jordan Bangia <george.doe231@gmail.com>
2013-10-25 12:50:47 -0400

    Added info icon, hooked up button, added text

--- src/com/jordanbang/musicdiff/MusicDiffMainActivity.java
+++ src/com/jordanbang/musicdiff/MusicDiffMainActivity.java
@@ -1,11 +1,13 @@
 package com.jordanbang.musicdiff;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 public class MusicDiffMainActivity extends Activity {
@@ -24,10 +26,12 @@ public class MusicDiffMainActivity extends Activity {
 		        MediaStore.Audio.Media.DISPLAY_NAME,
 		        MediaStore.Audio.Media.DURATION,
 		        MediaStore.Audio.Media.ALBUM,
-		        MediaStore.Audio.Media.IS_MUSIC};
+		        MediaStore.Audio.Media.IS_MUSIC,
+		        MediaStore.Audio.Media.TRACK};
 		
+		String sortOrder = MediaStore.Audio.Media.ARTIST + " ASC, " + MediaStore.Audio.Media.ALBUM + " ASC, " + MediaStore.Audio.Media.TRACK + " ASC";
 		@SuppressWarnings("deprecation")
-		Cursor music = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, proj, MediaStore.Audio.Media.IS_MUSIC + " <> 0", null, null);
+		Cursor music = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, proj, MediaStore.Audio.Media.IS_MUSIC + " <> 0", null, sortOrder);
 		
 		String[] projection = {MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ARTIST, MediaStore.Audio.Media.ALBUM};
 		int[] ids = {R.id.list_item_title, R.id.list_item_artist, R.id.list_item_album};
@@ -39,6 +43,22 @@ public class MusicDiffMainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.music_diff_main, menu);
 		return true;
-	}
+	}	
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		switch (id) {
+		case R.id.action_info:
+			createInfoDialog();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void createInfoDialog() {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle(R.string.welcome);
+		alert.setMessage(R.string.welcome_msg);
+		alert.show();
+	}
 }
